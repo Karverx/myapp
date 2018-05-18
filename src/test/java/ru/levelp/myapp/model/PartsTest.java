926f0322e4ce4003d8a5279e04316a52d5b08966
karverx <karverx@gmail.com>
2018-05-19 00:24:47 +0300

    Add PartsDAO

--- src/test/java/ru/levelp/myapp/model/PartsTest.java
+++ src/test/java/ru/levelp/myapp/model/PartsTest.java
@@ -3,6 +3,7 @@ package ru.levelp.myapp.model;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import ru.levelp.myapp.dao.PartsDAO;
 
 import javax.persistence.*;
 
@@ -17,11 +18,14 @@ public class PartsTest {
     private EntityManagerFactory emf;
     private EntityManager em;
 
+    private PartsDAO dao;
+
 
     @Before
     public void setup() {
         emf = Persistence.createEntityManagerFactory("TestPersistenceUnit");
         em = emf.createEntityManager();
+        dao = new PartsDAO(em);
     }
 
     @After
@@ -30,82 +34,32 @@ public class PartsTest {
         emf.close();
     }
 
-    @Test
-    public void testCreatePart() throws Throwable {
-        Part part = new Part("0000-1", "My part");
-        em.getTransaction().begin();
-        try {
-            em.persist(part);
-            em.getTransaction().commit();
-        } catch (Throwable t) {
-            em.getTransaction().rollback();
-            throw t;
-        }
-    }
 
     @Test
     public void testCreatePartWithSupplier() throws Throwable {
-
-        Supplier s1 = new Supplier();
-        s1.getParts().add(new Part("aaa", "aaaaa"));
-
-        em.persist(s1);
-
-
-        Part part = new Part("0000-1", "My part");
-        Supplier s = new Supplier();
-        s.setName("some company");
-        part.setSupplier(s);
+        Part part;
 
         em.getTransaction().begin();
         try {
-            em.persist(s);
-            em.persist(part);
+            part = dao.createPart("0000-1", "some part", dao.createSupplier("some company"));
             em.getTransaction().commit();
         } catch (Throwable t) {
             em.getTransaction().rollback();
             throw t;
         }
 
-        Part found = em.find(Part.class, part.getId());
+        Part found = dao.findByPrimaryKey(part.getId());
         assertNotNull(found);
         assertNotNull(found.getSupplier());
     }
 
-
-
     @Test
-    public void testQuery() throws Throwable {
+    public void testSearchByPartId() throws Throwable {
         testCreatePartWithSupplier();
 
         String searchString = "0000-1";
 
-        em.createNamedQuery("MyQuery");
-
-
-        @SuppressWarnings("unchecked")
-        List<Part> parts = em.createQuery("from Part where partId = :partId")
-                .setParameter("partId", searchString)
-                .getResultList();
-
-        assertEquals(1, parts.size());
-        Part found = parts.get(0);
-        assertEquals(searchString, found.getPartId());
-    }
-
-    @Test
-    public void testNamedQuery() throws Throwable {
-        testCreatePartWithSupplier();
-
-        String searchString = "0000-1";
-
-        em.createNamedQuery("MyQuery");
-
-
-        @SuppressWarnings("unchecked")
-        List<Part> parts = em.createNamedQuery(SEARCH_BY_PART_ID)
-                .setParameter("partId", searchString)
-                .getResultList();
+        List<Part> parts = dao.findByPartId(searchString);
 
         assertEquals(1, parts.size());
         Part found = parts.get(0);
